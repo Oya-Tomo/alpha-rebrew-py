92ff8e7d1779956735e4c2de04191b7f5262c92d
oyatomo <oyatomo.dev@gmail.com>
2024-08-02 23:41:22 +0900

    update: delete e-greedy

--- src/agent.py
+++ src/agent.py
@@ -1,7 +1,6 @@
 import copy
 import random
 from dataclasses import dataclass
-import torch
 import numpy as np
 
 from bitboard import Board, Stone
@@ -20,39 +19,36 @@ class ModelAgent:
     #     MCTS + e-greedy decision process
     #     buffer: [(state, turn, policy)]
 
-    def __init__(self, stone: Stone, mct: MCT, sim: int, eps: float) -> None:
+    def __init__(self, stone: Stone, mct: MCT, mcts_num: int) -> None:
         self.stone = stone
         self.mct = mct
-        self.sim = sim
-        self.eps = eps
+        self.mcts_num = mcts_num
 
         self.buffer: list[Step] = []  # (state, policy)
 
     def act(self, board: Board) -> int:
         actions = board.get_actions(self.stone)
 
-        policy = self.mct.search(board, self.stone, self.sim)
+        policy = self.mct.search(board, self.stone, self.mcts_num)
         self.buffer.append(
             Step(state=copy.deepcopy(board), turn=self.stone, policy=policy)
         )
         policy = np.array(policy, dtype=np.float32)
 
-        if self.eps > random.random():
-            action = random.choice(actions)
-        else:
-            while True:
-                action = int(random.choice(np.where(policy == policy.max())[0]))
+        while True:
+            action = int(random.choice(np.where(policy == policy.max())[0]))
 
-                if action in actions:
-                    break
-                policy[action] = 0
+            if action in actions:
+                break
+            policy[action] = 0
 
         return action
 
+    # will be deleted
     def act_random(self, board: Board) -> int:
         actions = board.get_actions(self.stone)
 
-        policy = self.mct.search(board, self.stone, self.sim)
+        policy = self.mct.search(board, self.stone, self.mcts_num)
         self.buffer.append(
             Step(state=copy.deepcopy(board), turn=self.stone, policy=policy)
         )
